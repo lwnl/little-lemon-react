2f721d395eb17306baf938d2e3fe173adfef4cbb
lwnl <liangwang.fr@gmail.com>
2025-06-26 15:49:56 +0200

    menuList

---
+++ src/data/menuList.js
@@ -0,0 +1,50 @@
+const menuList = [
+  {
+    title: "Bruschetta",
+    price: 8.0,
+    discription:
+      "Crisp fluffy bruschetta bread, topped with melted parmesan cheese, diced roma tomatoes, and freshly cut basil.",
+    imageUrl:
+      "http://www.little-lemon-restaurant.com/static/img/Bruschetta.jpg",
+  },
+  {
+    title: "Grilled Salmon",
+    price: 24.0,
+    discription:
+      "Fresh salmon, grilled to perfection, seasoned with lemon, black pepper, and rosemary.",
+    imageUrl:
+      "http://www.little-lemon-restaurant.com/static/img/Grilled%20Salmon.jpg",
+  },
+  {
+    title: "Grilled Trout",
+    price: 16.0,
+    discription:
+      "Whole grilled trout, seasoned with salt, pepper, and paired with lemon.",
+    imageUrl:
+      "http://www.little-lemon-restaurant.com/static/img/Grilled%20Trout.jpg",
+  },
+  {
+    title: "Lemon Cheesecake",
+    price: 8.0,
+    discription:
+      "Fluffy layered lemon cheesecake topped with whipped cream",
+    imageUrl:
+      "http://www.little-lemon-restaurant.com/static/img/Lemon%20Cheesecake.jpg",
+  },
+  {
+    title: "Tomato Basil Penne Pasta",
+    price: 18.0,
+    discription:
+      "Penne Pasta tossed with a creamy tomato basil sauce.",
+    imageUrl:
+      "http://www.little-lemon-restaurant.com/static/img/Tomato%20Basil%20Penne%20Pasta.jpg",
+  },
+  {
+    title: "Greek Salad",
+    price: 10.0,
+    discription:
+      "Fresh greek salad with tomato, olive, bell pepper, red onion, feta cheese, and cucumber.",
+    imageUrl:
+      "http://www.little-lemon-restaurant.com/static/img/Greek%20Salad.jpg",
+  },
+];
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,6 +1,15 @@
+import './Menu.css'
+
 const Menu = () => {
   return (
-    <></>
+    <div className="menu-container">
+      <h2>Menu</h2>
+      <div className='menu'>
+        {Array.from({length:6}).map((_, idx) => (
+          <div className="menu-item" key={idx}></div>
+        ))}
+      </div>
+    </div>
   )
 }
 
